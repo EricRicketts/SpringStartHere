4bce24d836610d949679b32db997abab0cbc8d7d
EricRicketts <eric_ricketts@me.com>
2021-12-06 09:53:57 -0500

    chapter 3 exercise 8 adding a bean with a parameter identifier

---
+++ mycode/chapter3/sq-ch3-ex8/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import beans.Parrot;
+import beans.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ProjectConfig {
+    @Bean
+    public Parrot parrot1() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Parrot parrot2() {
+        Parrot p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+
+    @Bean
+    public Person person(Parrot parrot2) {
+        Person p = new Person();
+        p.setName("Ella");
+        p.setParrot(parrot2);
+        return p;
+    }
+}
