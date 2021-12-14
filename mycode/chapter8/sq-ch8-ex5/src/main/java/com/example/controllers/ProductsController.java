9abcd0da2809f1eb7dfe757121dd349d8a156b40
EricRicketts <eric_ricketts@me.com>
2021-12-14 17:47:54 -0500

    chapter 8 exercise 5 GET and POST methods in a Spring application

---
+++ mycode/chapter8/sq-ch8-ex5/src/main/java/com/example/controllers/ProductsController.java
@@ -0,0 +1,43 @@
+package com.example.controllers;
+
+import com.example.models.Product;
+import com.example.services.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductsController {
+
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(
+        @RequestParam String name,
+        @RequestParam double price,
+        Model model
+    ) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+}
