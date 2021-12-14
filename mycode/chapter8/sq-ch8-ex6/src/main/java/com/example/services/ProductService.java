941d33ac63b9974793813cc8434e08924b26b7f4
EricRicketts <eric_ricketts@me.com>
2021-12-14 17:56:22 -0500

    chapter 8 exercise 6 shorten POST parameters in the controller

---
+++ mycode/chapter8/sq-ch8-ex6/src/main/java/com/example/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.example.services;
+
+import com.example.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product p) {
+        products.add(p);
+    }
+
+    public List<Product> findAll() {
+        return products;
+    }
+}
