f67b91703e618261d45f57afa4973e27f995cb17
EricRicketts <eric_ricketts@me.com>
2021-12-15 10:30:39 -0500

    chapter 10 exercise 4 return a ResponseEntity from the REST endpoint, set response headers in addition to returning a response body

---
+++ mycode/chapter10/sq-ch10-ex4/src/main/java/com/example/controllers/CountryController.java
@@ -0,0 +1,24 @@
+package com.example.controllers;
+
+import com.example.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    @GetMapping("/france")
+    public ResponseEntity<Country> france() {
+        Country c = Country.of("France", 67);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .header("continent", "Europe")
+                .header("capital", "Paris")
+                .header("favorite_food", "cheese and wine")
+                .body(c);
+    }
+}
