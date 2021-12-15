9ddce04dfd1f7955e4f8b72251605ec502658244
EricRicketts <eric_ricketts@me.com>
2021-12-15 10:10:44 -0500

    chapter 10 exercise 3 Spring REST implementation JSON formatted return

---
+++ mycode/chapter10/sq-ch10-ex3/src/main/java/com/example/model/Country.java
@@ -0,0 +1,31 @@
+package com.example.model;
+
+public class Country {
+
+    private String name;
+    private int population;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public static Country of(String name, int population) {
+        Country country = new Country();
+        country.setName(name);
+        country.setPopulation(population);
+
+        return country;
+    }
+}
