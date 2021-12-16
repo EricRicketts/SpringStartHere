e8af9a58e6bf92220221219998b24fb934c326fb
EricRicketts <eric_ricketts@me.com>
2021-12-16 13:54:16 -0500

    chapter 11 exercise 2 service a REST endpoint using RestTemplate

---
+++ mycode/chapter11/sq-ch11-ex2/src/main/java/com/example/proxy/PaymentsProxy.java
@@ -0,0 +1,41 @@
+package com.example.proxy;
+
+import com.example.model.Payment;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+@Component
+public class PaymentsProxy {
+
+    private final RestTemplate rest;
+
+    @Value("${name.service.url}")
+    private String paymentsServiceUrl;
+
+    public PaymentsProxy(RestTemplate rest) {
+        this.rest = rest;
+    }
+
+    public Payment createPayment(Payment payment) {
+        String uri = paymentsServiceUrl + "/payment";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("requestId", UUID.randomUUID().toString()) ;
+
+        HttpEntity<Payment> httpEntity = new HttpEntity<>(payment, headers);
+
+        ResponseEntity<Payment> response =
+                rest.exchange(uri,
+                        HttpMethod.POST,
+                        httpEntity,
+                        Payment.class);
+
+        return response.getBody();
+    }
+}
