dfdaea27d62bf0dae54682a96ff749c73f42986a
EricRicketts <eric_ricketts@me.com>
2021-12-17 14:22:20 -0500

    chapter 12 exercise 2

--- mycode/chapter12/sq-ch12-ex2/src/main/java/com/example/repositories/PurchaseRepository.java
+++ mycode/chapter12/sq-ch12-ex2/src/main/java/com/example/repositories/PurchaseRepository.java
@@ -10,28 +10,28 @@ import java.util.List;
 @Repository
 public class PurchaseRepository {
 
-    private final JdbcTemplate jdbc;
+  private final JdbcTemplate jdbc;
 
-    public PurchaseRepository(JdbcTemplate jdbc) {
-        this.jdbc = jdbc;
-    }
+  public PurchaseRepository(JdbcTemplate jdbc) {
+    this.jdbc = jdbc;
+  }
 
-    public void storePurchase(Purchase purchase) {
-        String sql = "INSERT INTO purchase VALUES (NULL, ?, ?)";
-        jdbc.update(sql, purchase.getProduct(), purchase.getPrice());
-    }
+  public void storePurchase(Purchase purchase) {
+    String sql = "INSERT INTO purchase VALUES (NULL, ?, ?)";
+    jdbc.update(sql, purchase.getProduct(), purchase.getPrice());
+  }
 
-    public List<Purchase> findAllPurchases() {
-        String sql = "SELECT * FROM purchase";
+  public List<Purchase> findAllPurchases() {
+    String sql = "SELECT * FROM purchase";
 
-        RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
-            Purchase rowObject = new Purchase();
-            rowObject.setId(r.getInt("id"));
-            rowObject.setProduct(r.getString("product"));
-            rowObject.setPrice(r.getBigDecimal("price"));
-            return rowObject;
-        };
+    RowMapper<Purchase> purchaseRowMapper = (r, i) -> {
+      Purchase rowObject = new Purchase();
+      rowObject.setId(r.getInt("id"));
+      rowObject.setProduct(r.getString("product"));
+      rowObject.setPrice(r.getBigDecimal("price"));
+      return rowObject;
+    };
 
-        return jdbc.query(sql, purchaseRowMapper);
-    }
+    return jdbc.query(sql, purchaseRowMapper);
+  }
 }
