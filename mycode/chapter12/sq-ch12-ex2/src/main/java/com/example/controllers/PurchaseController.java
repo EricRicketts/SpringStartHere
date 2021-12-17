0c89f412cb20a44276460fa6d26f71d80131d612
EricRicketts <eric_ricketts@me.com>
2021-12-17 14:21:33 -0500

    chapter 12 exercise 1

---
+++ mycode/chapter12/sq-ch12-ex2/src/main/java/com/example/controllers/PurchaseController.java
@@ -0,0 +1,28 @@
+package com.example.controllers;
+
+import com.example.model.Purchase;
+import com.example.repositories.PurchaseRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/purchase")
+public class PurchaseController {
+
+    private final PurchaseRepository purchaseRepository;
+
+    public PurchaseController(PurchaseRepository purchaseRepository) {
+        this.purchaseRepository = purchaseRepository;
+    }
+
+    @PostMapping
+    public void storePurchase(@RequestBody Purchase purchase) {
+        purchaseRepository.storePurchase(purchase);
+    }
+
+    @GetMapping
+    public List<Purchase> findPurchases() {
+        return purchaseRepository.findAllPurchases();
+    }
+}
