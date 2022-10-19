35465727cdd8372f917560bb6115ef7542cbc899
Eric Ricketts <eric_ricketts@me.com>
2022-10-19 18:26:55 -0400

    sq-ch2-ex4 partially done

---
+++ mycode-redo/chapter2/sq-ch2-ex4/src/main/java/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import main.Parrot;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+  @Bean(name = "koko")
+  Parrot parrot1() {
+    var p = new Parrot();
+    p.setName("Koko");
+    return p;
+  }
+
+  @Bean(name = "miki")
+  Parrot parrot2() {
+    var p = new Parrot();
+    p.setName("Miki");
+    return p;
+  }
+
+  @Bean(name = "riki")
+  Parrot parrot3() {
+    var p = new Parrot();
+    p.setName("Riki");
+    return p;
+  }
+}
