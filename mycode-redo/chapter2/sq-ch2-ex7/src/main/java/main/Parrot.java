293be31ad9c21ab8a6739abec08bb92735482854
EricRicketts <eric_ricketts@me.com>
2022-10-20 12:46:31 -0400

    sq-ch2-ex7 add @PostConstruct to stereotype annotation to do something with instance after it is added to the context

---
+++ mycode-redo/chapter2/sq-ch2-ex7/src/main/java/main/Parrot.java
@@ -0,0 +1,23 @@
+package main;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+public class Parrot {
+  private String name;
+
+  @PostConstruct
+  public void init() {
+    this.setName("Kiki");
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ mycode-redo/chapter2/sq-ch2-ex7/src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import config.ProjectConfig;
+import main.Parrot;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main( String[] args ) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        var p = context.getBean(Parrot.class);
+        System.out.println(p.getName());
+    }
+}
