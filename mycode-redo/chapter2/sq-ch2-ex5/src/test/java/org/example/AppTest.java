ff82836b1464aeb6ce80be3c39c3dd3b68fbad8b
EricRicketts <eric_ricketts@me.com>
2022-10-20 10:22:25 -0400

    sq-ch2-ex5 add test for designating primary bean using @Primary annotation

--- mycode-redo/chapter2/sq-ch2-ex5/src/test/java/org/example/AppTest.java
+++ mycode-redo/chapter2/sq-ch2-ex5/src/test/java/org/example/AppTest.java
@@ -1,38 +1,44 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import config.ProjectConfig;
+import main.Parrot;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration( classes = { ProjectConfig.class })
+public class AppTest {
+  private String expected;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  @Autowired
+  private ApplicationContext context;
+
+  @Test
+  @DisplayName("there is an option to designate a primary bean for a context")
+  public void testContextHasPrimaryBean() {
+    expected = "Koko";
+    var p = context.getBean(Parrot.class);
+    assertEquals(expected, p.getName());
+  }
+  @Test
+  @DisplayName("there is an option to designate a bean by a method name for a context")
+  public void testContextHasBeanByMethodName() {
+    expected = "Miki";
+    var p = context.getBean("parrot2", Parrot.class);
+    assertEquals(expected, p.getName());
+  }
+  @Test
+  @DisplayName("there is an option to designate a bean by @Bean annotation arguments for a context")
+  public void testContextHasBeanByAnnotationName() {
+    expected = "Riki";
+    var p = context.getBean("riki", Parrot.class);
+    assertEquals(expected, p.getName());
+  }
 }
