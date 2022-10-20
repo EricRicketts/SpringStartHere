ba15f38fc4e8437ed49b5e7dee016b7d7baee1f9
EricRicketts <eric_ricketts@me.com>
2022-10-20 09:40:55 -0400

    sq-ch2-ex4 if the arguments are used in the @Bean annotation to identify a bean the method name cannot be used to identify the bean

--- mycode-redo/chapter2/sq-ch2-ex4/src/test/java/org/example/AppTest.java
+++ mycode-redo/chapter2/sq-ch2-ex4/src/test/java/org/example/AppTest.java
@@ -5,12 +5,13 @@ import main.Parrot;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {ProjectConfig.class})
@@ -43,5 +44,13 @@ public class AppTest {
     expected = "Riki";
     var p = context.getBean("riki", Parrot.class);
     assertEquals(expected, p.getName());
+  }
+
+  @Test
+  @DisplayName("with name given in bean argument context does not allow bean identification by method name")
+  public void testParrotBeanNamedParrot1() {
+    assertThrows(NoSuchBeanDefinitionException.class, () -> {
+        context.getBean("parrot1", Parrot.class);
+    }, "NoSuchBeanDefinition was expected");
   }
 }
