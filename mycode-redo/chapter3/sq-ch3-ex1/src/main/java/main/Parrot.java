9c2ab7e89b29be13a0b56bf4bbee7e49fbf43a1a
Eric Ricketts <eric_ricketts@me.com>
2022-10-20 19:24:00 -0400

    Add some beans to the context in sq-ch3-ex1

--- mycode-redo/chapter3/sq-ch3-ex1/src/main/java/main/Parrot.java
+++ mycode-redo/chapter3/sq-ch3-ex1/src/main/java/main/Parrot.java
@@ -1,5 +1,18 @@
 package main;
 
 public class Parrot {
-  
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Parrot: " + this.getName();
+  }
 }
