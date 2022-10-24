f8fa5fb105c8160b903daf54028368d4a0db8525
Eric Ricketts <eric_ricketts@me.com>
2022-10-24 12:28:21 -0400

    clean up rest of duplicates

--- mycode/chapter4/sq-ch4-ex5/src/main/java/configuration/ProjectConfiguration 3.java
+++
@@ -1,9 +0,0 @@
-package configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ComponentScan;
-
-@Configuration
-@ComponentScan(basePackages = {"proxies", "repositories", "services"})
-public class ProjectConfiguration {
-}
